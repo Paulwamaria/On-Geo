edc2e649116403db7d95cb23116b5dd3497582b1
Paulwamaria <paulwamaria@gmail.com>
2019-10-30 16:31:23 +0300

    Register class Profile

--- ongeo/admin.py
+++ ongeo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from.models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(OSMGeoAdmin):
+    list_display = ('user', 'location')
