c3592f8ed5bcd338b77546aeee1d6201f4b45644
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 02:54:48 +0300

    Register the createand save profile signals

--- ongeo/apps.py
+++ ongeo/apps.py
@@ -1,5 +1,10 @@
+from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
 class OngeoConfig(AppConfig):
     name = 'ongeo'
+
+
+    def ready(self):
+        import ongeo.signals
