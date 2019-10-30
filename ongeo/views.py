6350f915f64e6111ea85a39aa46e9282975a29a9
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 02:53:41 +0300

    Add a register funtion and anotherone to update profile

--- ongeo/views.py
+++ ongeo/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import OnGeoRegistrationForm,UserUpdateForm,ProfileUpdateForm
 
 def index(request):
     message = "Welcome to On-Geo Manager"
@@ -8,3 +12,56 @@ def index(request):
         "message":message
     }
     return render(request,"ongeo/index.html", context)
+
+def register(request):
+    if request.method == 'POST':
+        form = OnGeoRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            messages.success(request, f'Account created for {username}')
+            return redirect('profile')
+    else:
+        form = OnGeoRegistrationForm()
+    return render(request, 'profile/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance = request.user)
+        
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)    
+        if u_form.is_valid and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'profile/profile.html',context)
+
+
+def display_profile(request,username):
+    profile = Profile.objects.get(user__username= username)
+
+    user_posts = Post.objects.filter(user =profile.user).order_by('created_on')
+
+    context={
+        "profile":profile,
+        "user_posts":user_posts
+    }
+    return render(request,'profile/profile_detail.html',context)
+
+
