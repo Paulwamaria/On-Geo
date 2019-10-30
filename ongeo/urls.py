3ff6baa677b2f378f211d0af3ee7f60cc57f7e4a
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 02:56:12 +0300

    Add urls for registering,login, logout and profile

--- ongeo/urls.py
+++ ongeo/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,8 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='home'),
+    path('register/',views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='profile/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='profile/logout.html'), name='logout'),
+    path('profile/',views.profile, name = 'profile'),
 ]
