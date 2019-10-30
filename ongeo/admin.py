85ab43a1d67e30c86dbc6db6c6ed8fd9e0d4240c
Paulwamaria <paulwamaria@gmail.com>
2019-10-30 18:23:42 +0300

    Register class organization

--- ongeo/admin.py
+++ ongeo/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from.models import Profile
+from.models import Profile, Organisation
 
 # Register your models here.
+@admin.register(Organisation)
+class OrganisationAdmin(OSMGeoAdmin):
+    list_display = ('organisation_name', 'location')
+
+
 @admin.register(Profile)
 class ProfileAdmin(OSMGeoAdmin):
     list_display = ('user', 'location')
