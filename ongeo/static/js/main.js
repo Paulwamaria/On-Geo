c1b0fd6128f0ce62a7881c06726ba88eb35c5da5
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 19:39:06 +0300

    Add a function to get user location and send data to  database

--- ongeo/static/js/main.js
+++ ongeo/static/js/main.js
@@ -1,13 +1,61 @@
-function isGeo(){
-    if("geolocation" in navigator){
-    console.log("geolocation is available");
-    document.getElementById("demo").innerHTML = "geolocation is available";
-}else{
-    console.log("Geolocation not available");
-    document.getElementById("demo").innerHTML = "geolocation is unavailable";
-}
+$(document).ready(function () {
+    navigator.geolocation.getCurrentPosition(onPositionUpdate);
+});
+
+
+function onPositionUpdate(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    $.ajax({
+            url: '/distance/',
+            data: {
+                'user_latitude': lat,
+                'user_longitude': long,
+            },
+            dataType: 'json'
+            // success : function() {
+            //     document.getElementById("demo").innerHTML = ("Your Latitude: "+ lat + " < " +" Your Longitude: " + long);
+            //     console.log("Your Latitude:"+ lat + " " +" Your Longitude:" + long);
+
+
+            // }
+        })
+        .done(function (data) {
+                nams = $("#lname").val(data.last_name)
+
+            if (data.distance < 1) {
+                $("#fname").val(data.user_data.first_name)
+                $("#lname").val(data.user_data.last_name)
+                console.log(data.user_data);
+                console.log("You are within Moringa Vicinity!");
+                $("ol#inattendance").prepend(data.user_data.first_name + " " + data.user_data.last_name);
+                $.ajax({
+                    url:'/save_to_db/',
+                    method:'GET',
+                    data:{
+                       'first_name':data.user_data.first_name,
+                        'last_name':data.user_data.last_name
+                    },
+                    statusCode:{
+                        404:function(){
+                            console.log('NOT FOUND') 
+                        },
+                        500:function(){
+                            console.log('INTERNAL SERVER ERROR') 
+                        }
+                    }
+                })
+                .done(function(data){
+                    if(data.saved){
+                        window.location.replace('/')
+                    }
+                })
+            } else if (data.distance > 1) {
+                document.getElementById("outb").innerHTML = 'We we are sorry the system cant check you in, you are not Within Moringa'
+                console.log("Sorry You are out of bound");
+            }
+        });
+    console.log(name);
+
 
 }
-navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position.coords.latitude, position.coords.longitude);
-  });
