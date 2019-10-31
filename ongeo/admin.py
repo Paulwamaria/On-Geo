87d2cf700dafb37844e5c724110d1b2762e889ae
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 15:09:24 +0300

    Register classes Post, and UserCoords

--- ongeo/admin.py
+++ ongeo/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from.models import Profile, Organisation
+from.models import Profile, Organisation,Post, UserCoords,Attendance
 
 # Register your models here.
 @admin.register(Organisation)
@@ -11,3 +11,18 @@ class OrganisationAdmin(OSMGeoAdmin):
 @admin.register(Profile)
 class ProfileAdmin(OSMGeoAdmin):
     list_display = ('user', 'location')
+
+@admin.register(Post)
+class PostAdmin(OSMGeoAdmin):
+    list_display = ('title', 'content')
+
+  
+@admin.register(UserCoords)
+class UserCoordsAdmin(OSMGeoAdmin):
+    # list_display = (' coords_long',' coords_long')
+    pass
+
+  
+@admin.register(Attendance)
+class AttendanceAdmin(OSMGeoAdmin):
+    pass
