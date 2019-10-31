bcaee4290cf25c510ca01c4a4a40f343e1f1853b
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 15:12:42 +0300

    Create classes Post , UserCoords and Attendance

--- ongeo/models.py
+++ ongeo/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Organisation(models.Model):
     organisation_name = models.CharField(max_length=60)
-    location= models.PointField(-1.3034531999999999, 36.7927116)
+    location= models.PointField()
     logo = models.ImageField(upload_to='media/', blank = True, null =True)
     members = models.PositiveIntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -28,7 +28,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic =  models.ImageField(upload_to='media/profile_pics', default='media/default.jpg')
     bio = models.TextField()
-    location= models.PointField()
+    organisation = models.CharField(max_length=60, blank=True,null=True)
+    location= models.PointField(null=True)
    
 
 
@@ -40,3 +41,63 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    organisation = models.ForeignKey(Organisation, related_name='posts', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/blog/', blank = True, null =True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+
+    def __str__(self):
+        return self.title
+
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+
+class UserCoords(models.Model):
+    coords_long = models.IntegerField()
+    coords_latt = models.IntegerField()
+    organisation = models.ForeignKey(Organisation, related_name='usercoords', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank = True)
+    
+
+
+    def __str__(self):
+        return f'{self. coords_long} {self.coords_latt}'
+
+
+    def save_usercoords(self):
+        self.save()
+
+    def delete_usercoords(self):
+        self.delete()
+
+
+class Attendance(models.Model):
+    organisation = models.ForeignKey(Organisation, related_name='inattendance', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+    def save_attendance(self):
+        self.save()
+
+    def delete_attendance(self):
+        self.delete()
