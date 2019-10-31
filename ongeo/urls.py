16b092cbd81581bf40ca7d4c381f87de20d598b3
Paulwamaria <paulwamaria@gmail.com>
2019-10-31 15:13:57 +0300

    Create urls for attendance, distance and profile details

--- ongeo/urls.py
+++ ongeo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static  import static
 from . import views
 
 
@@ -11,4 +12,10 @@ urlpatterns=[
     path('login/', auth_views.LoginView.as_view(template_name='profile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='profile/logout.html'), name='logout'),
     path('profile/',views.profile, name = 'profile'),
+    path('attendance/',views.attendance, name = 'attendance'),
+    path('distance/',views.get_distance, name = 'get-distance'),
+    path('profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
