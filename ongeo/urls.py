0d84b8cbf4dd3f8d654c7a52a13781411a63d12e
Paulwamaria <paulwamaria@gmail.com>
2019-11-01 05:47:09 +0300

    Add urls for about, posts, post create,post delete and no-setup

--- ongeo/urls.py
+++ ongeo/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static  import static
+from .views import PostCreateView,NotificationCreateView,PostUpdateView,PostDeleteView
 from . import views
 
 
 
 urlpatterns=[
     path('',views.index,name='home'),
+    path('about/',views.about,name='about'),
+     path('setup/',views.no_setup,name='setup'),
+    path('posts/',views.post,name='posts'),
     path('register/',views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='profile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='profile/logout.html'), name='logout'),
@@ -16,6 +20,10 @@ urlpatterns=[
     path('save_to_db/',views.save_to_db, name = 'save-to-db'),
     path('distance/',views.get_distance, name = 'get-distance'),
     path('profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
+    path('post/new/',PostCreateView.as_view(), name = 'post-create'),
+    path('notification/new/',NotificationCreateView.as_view(), name = 'notification-create'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name = 'post-delete'),
 ]
 
 if settings.DEBUG:
