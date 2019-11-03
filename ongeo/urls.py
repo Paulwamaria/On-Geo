0693faddf3e02f9f8961bd021e2a72db3e560022
Paulwamaria <paulwamaria@gmail.com>
2019-11-04 01:12:10 +0300

    Add url for changing community

--- ongeo/urls.py
+++ ongeo/urls.py
@@ -26,6 +26,7 @@ urlpatterns=[
     path('notification/new/',NotificationCreateView.as_view(), name = 'notification-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(), name = 'post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(), name = 'post-delete'),
+    path('community/change/',views.switch_community, name = 'switch-community'),
 ]
 
 if settings.DEBUG:
